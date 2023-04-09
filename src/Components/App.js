50b5e0a26e4684dc452bdb37501c5dd51508ae1a
Sourav <mehtas018@gmail.com>
2023-04-09 23:29:37 +0530

    day2 of figuring shit out

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,14 @@ import React from "react"
 import CtBtn from "./CounterButton"
 
 function App() {
-  
+  /*
+  <CtBtn />   
+  This is the command used inside return button to get the output with value assigned while defining function */
     return(
-        <div>             
-          <CtBtn /> 
-          jkjk
+        <div>           
+          <CtBtn incrementBy={1} buttonColor={"pink"} brdr={"2px"} />
+          <CtBtn incrementBy={5} buttonColor={"white"} brdr={"2px"} />
+          <CtBtn incrementBy={7} buttonColor={"yellow"}brdr={"2px"} />
         </div>
       
     )
--- src/Components/CounterButton.js
+++ src/Components/CounterButton.js
@@ -2,18 +2,29 @@ import React, {useState} from "react";
 
 /*let CC=10  Cuttent Count*/
 
-function CtBtn() {
+function CtBtn(props) {
     const [CC, setCC]=useState(0) /* In react we can not simply add to a variable and expext
                              it to display the latest data stored, so we have to create an
                              array and mark it as "useState" displaying it's importance in 
                              output of code. No we can not define these 2 as use state variables
                              separately*/
+    console.log(props.incrementBy)
     function add() {
-        setCC(CC+11)
+      /*  setCC(CC+11)      ~this was the code with pre assigned value for each time function is <called></called> */
+      setCC(CC + props.incrementBy) /*this is the code where value in function can be set wile calling it */
     }
+    const btnstyl = {
+        background : props.buttonColor,
+        borderRadius : props.brdr
+    }
+    const DivStyle = {
+        color :"blue",
+        border : "10px solid black",
+        borderRadius : "14px"
+    } // Or instead of defining new function we can write all that <div style= {{here}}>
     return (
-    <div> 
-        <button onClick={add}> +1 </button>
+    <div style = {DivStyle}> 
+        <button style = {btnstyl} onClick={add}> +{props.incrementBy} </button>
         <div> {CC}</div>
     </div>           
     )
